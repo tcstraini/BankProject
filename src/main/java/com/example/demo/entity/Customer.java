8a1d827f768cfadd5443634b8dc3ea299cdccbe6
tcstraini <tcstraini@gmail.com>
2022-01-21 13:50:36 +0530

    updated

--- src/main/java/com/example/demo/entity/Customer.java
+++ src/main/java/com/example/demo/entity/Customer.java
@@ -4,96 +4,30 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
-
-import com.sun.istack.NotNull;
 
 @Entity
 public class Customer {
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
-	@NotBlank
-	@NotNull
-	@Size(max = 25)
-	@Pattern(regexp ="^[a-zA-Z\s]+$")
 	private String name;
-	@NotBlank
-	@NotNull
 	private String gender;
-	@NotBlank
-	@NotNull
-	@Pattern(regexp = "^(([0-9])|([0-2][0-9])|([3][0-1]))\\-(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\\-\\d{4}$")
 	private String dob;
-	@NotBlank
-	@NotNull
-	@Size(max = 50)
 	private String address;
-	@NotBlank
-	@NotNull
-	@Size(max = 25)
-	@Pattern(regexp ="^[a-zA-Z\s]+$")
 	private String city;
-	@NotBlank
-	@NotNull
-	@Size(max = 25)
-	@Pattern(regexp ="^[a-zA-Z\s]+$")
 	private String state;
-	@NotBlank
-	@NotNull
-	@Pattern(regexp = "[0-9 ]+")
-	@Size(max = 7)
-	private String pin;
-	@Pattern(regexp = "[0-9 ]+")
-	@Size(max = 15)
-	private String tel;
-	@Size(max = 15)
-	@Pattern(regexp = "[0-9 ]+")
-	private String fax;
-	@Email
-	@NotNull
-	@NotBlank
-	@Size(max = 30)
-	@Pattern(regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+	private Integer pin;
+	private Integer telephone;
+	private Integer fax;
 	private String email;
 	
-	public Customer(){
-		
-	}
 	
-	public String getState() {
-		return state;
+
+	public Integer getId() {
+		return id;
 	}
-	public void setState(String state) {
-		this.state = state;
-	}
-	public String getPin() {
-		return pin;
-	}
-	public void setPin(String pin) {
-		this.pin = pin;
-	}
-	public String getTel() {
-		return tel;
-	}
-	public void setTel(String tel) {
-		this.tel = tel;
-	}
-	public String getFax() {
-		return fax;
-	}
-	public void setFax(String fax) {
-		this.fax = fax;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
+	public void setId(Integer id) {
+		this.id = id;
 	}
 	
 	public String getName() {
@@ -126,4 +60,36 @@ public class Customer {
 	public void setCity(String city) {
 		this.city = city;
 	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public Integer getPin() {
+		return pin;
+	}
+	public void setPin(Integer pin) {
+		this.pin = pin;
+	}
+	public Integer getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(Integer telephone) {
+		this.telephone = telephone;
+	}
+	public Integer getFax() {
+		return fax;
+	}
+	public void setFax(Integer fax) {
+		this.fax = fax;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 }
